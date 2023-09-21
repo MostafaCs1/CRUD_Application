970aafba4233a5a13b1cd9729acf7f0b129abf82
Mostafa Mortezapour <mostafamortezapour1@gmail.com>
2023-09-21 21:09:03 +0330

    Add ``Upload countries from excel files`` into country services

--- ServiceContracts/ICountriesService.cs
+++ ServiceContracts/ICountriesService.cs
@@ -1,27 +1,33 @@
 ﻿using ServiceContracts.DTO;
+using Microsoft.AspNetCore.Http;
 
-namespace ServiceContracts
+namespace ServiceContracts;
+public interface ICountriesService
 {
-    public interface ICountriesService
-    {
-        /// <summary>
-        /// Add given country object to coutries
-        /// </summary>
-        /// <param name="countryAddRequest">country object to add</param>
-        /// <returns>return corresponding country object as country response</returns>
-        Task<CountryResponse> AddCountry(CountryAddRequest? countryAddRequest);
+    /// <summary>
+    /// Add given country object to coutries
+    /// </summary>
+    /// <param name="countryAddRequest">country object to add</param>
+    /// <returns>return corresponding country object as country response</returns>
+    Task<CountryResponse> AddCountry(CountryAddRequest? countryAddRequest);
 
-        /// <summary>
-        /// Get all countries in list of countries
-        /// </summary>
-        /// <returns>return all counties as list of country response</returns>
-        Task<List<CountryResponse>> GetAllCountries();
+    /// <summary>
+    /// Get all countries in list of countries
+    /// </summary>
+    /// <returns>return all counties as list of country response</returns>
+    Task<List<CountryResponse>> GetAllCountries();
 
-        /// <summary>
-        /// Get country object by country name
-        /// </summary>
-        /// <param name="countryID">CountryID (guid) to search</param>
-        /// <returns>Matching country as CountryResponse object</returns>
-        Task<CountryResponse?> GetCountryByCountryID(Guid? countryID);
-    }
+    /// <summary>
+    /// Get country object by country name
+    /// </summary>
+    /// <param name="countryID">CountryID (guid) to search</param>
+    /// <returns>Matching country as CountryResponse object</returns>
+    Task<CountryResponse?> GetCountryByCountryID(Guid? countryID);
+
+    /// <summary>
+    /// Upload countries from excel file into database
+    /// </summary>
+    /// <param name="formFile">Excel file with the list of countries</param>
+    /// <returns>Return number of countries that added</returns>
+    Task<int> UploadCountriesFromExcelFile(IFormFile formFile);
 }
--- Services/CountriesService.cs
+++ Services/CountriesService.cs
@@ -2,71 +2,104 @@
 using ServiceContracts.DTO;
 using Microsoft.EntityFrameworkCore;
 using Entities;
+using Microsoft.AspNetCore.Http;
+using OfficeOpenXml;
 
-namespace Services
+namespace Services;
+
+public class CountriesService : ICountriesService
 {
-    public class CountriesService : ICountriesService
+    //private fileds
+    private readonly PersonDbContext _db;
+
+    //constractor
+    public CountriesService(PersonDbContext personDbContext)
     {
-        //private fileds
-        private readonly PersonDbContext _db;
+        _db = personDbContext;
+    }
 
-        //constractor
-        public CountriesService(PersonDbContext personDbContext)
+    //Services
+    public async Task<CountryResponse> AddCountry(CountryAddRequest? countryAddRequest)
+    {
+        //validation : country add request can't be null
+        if (countryAddRequest == null)
         {
-            _db = personDbContext;
+            throw new ArgumentNullException(nameof(countryAddRequest));
         }
 
-        //Services
-        public async Task<CountryResponse> AddCountry(CountryAddRequest? countryAddRequest)
+        //validation : country add request's name can't be null
+        if (string.IsNullOrEmpty(countryAddRequest.CountryName))
         {
-            //validation : country add request can't be null
-            if (countryAddRequest == null)
+            throw new ArgumentException(nameof(countryAddRequest.CountryName));
+        }
+
+        //validation : country name can't be duplicate
+        if (await _db.Countries.AnyAsync(country => country.CountryName == countryAddRequest.CountryName))
+        {
+            throw new ArgumentException("Given country name is already exist.");
+        }
+
+        //change country add request to country object
+        Country newCountry = countryAddRequest.ToCountry();
+
+        //generate new countryId 
+        newCountry.CountryID = Guid.NewGuid();
+
+        //add country to country list
+        _db.Countries.Add(newCountry);
+        await _db.SaveChangesAsync();
+
+        return newCountry.ToCountryResponse();
+    }
+
+    public async Task<List<CountryResponse>> GetAllCountries()
+    {
+        //return all country that exist in countries list as list of response type
+        return await _db.Countries.Select(country => country.ToCountryResponse()).ToListAsync();
+    }
+
+    public async Task<CountryResponse?> GetCountryByCountryID(Guid? countryID)
+    {
+        if (countryID == null)
+            return null;
+
+        Country? country = await _db.Countries.FirstOrDefaultAsync(temp => temp.CountryID == countryID);
+
+        if (country == null)
+            return null;
+
+        return country.ToCountryResponse();
+    }
+
+    public async Task<int> UploadCountriesFromExcelFile(IFormFile formFile)
+    {
+        var memoryStream = new MemoryStream();
+        await formFile.CopyToAsync(memoryStream);
+        int countInsertedCountry = 0;
+
+        using (var excelPackage = new ExcelPackage(memoryStream))
+        {
+            ExcelWorksheet countriesSheet = excelPackage.Workbook.Worksheets["Countries"];
+
+            int totalRow = countriesSheet.Dimension.Rows;
+            for (int row = 2; row < totalRow; row++)
             {
-                throw new ArgumentNullException(nameof(countryAddRequest));
+                object? cellValue = countriesSheet.Cells[row, 1].Value;
+                if(cellValue != null)
+                {
+                    string? countryName = cellValue.ToString();
+                    if(!_db.Countries.Where(temp => temp.CountryName == countryName).Any())
+                    {
+                        var country = new Country() { CountryID = Guid.NewGuid(), CountryName = countryName};
+                        _db.Countries.Add(country);
+                        await _db.SaveChangesAsync();
+
+                        countInsertedCountry++;
+                    }
+                }
             }
-
-            //validation : country add request's name can't be null
-            if (string.IsNullOrEmpty(countryAddRequest.CountryName))
-            {
-                throw new ArgumentException(nameof(countryAddRequest.CountryName));
-            }
-
-            //validation : country name can't be duplicate
-            if (await _db.Countries.AnyAsync(country => country.CountryName == countryAddRequest.CountryName))
-            {
-                throw new ArgumentException("Given country name is already exist.");
-            }
-
-            //change country add request to country object
-            Country newCountry = countryAddRequest.ToCountry();
-
-            //generate new countryId 
-            newCountry.CountryID = Guid.NewGuid();
-
-            //add country to country list
-            _db.Countries.Add(newCountry);
-            await _db.SaveChangesAsync();
-
-            return newCountry.ToCountryResponse();
         }
 
-        public async Task<List<CountryResponse>> GetAllCountries()
-        {
-            //return all country that exist in countries list as list of response type
-            return await _db.Countries.Select(country => country.ToCountryResponse()).ToListAsync();
-        }
-
-        public async Task<CountryResponse?> GetCountryByCountryID(Guid? countryID)
-        {
-            if (countryID == null)
-                return null;
-
-            Country? country = await _db.Countries.FirstOrDefaultAsync(temp => temp.CountryID == countryID);
-
-            if (country == null)
-                return null;
-
-            return country.ToCountryResponse();
-        }
+        return countInsertedCountry;
     }
 }
