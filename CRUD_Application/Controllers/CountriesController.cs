3261abc95d88246c0930950d16b17e6760f7dacb
Mostafa Mortezapour <mostafamortezapour1@gmail.com>
2023-09-22 11:31:07 +0330

    Add `UploadCountriesFromExcelFile` view

---
+++ CRUD_Application/Controllers/CountriesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_Application.Controllers;
+
+[Route("[Controller]")]
+public class CountriesController : Controller
+{
+    [HttpGet]
+    [Route("[action]")]
+    public IActionResult UploadCountriesFromExcelFile()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [Route("[action]")]
+    public IActionResult UploadCountriesFromExcelFile(IFormFile formFile)
+    {
+        return View();
+    }
+}
