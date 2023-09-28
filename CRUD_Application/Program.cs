3dfde58e9521e237058d87f2ec071beea7ee22e9
Mostafa Mortezapour <mostafamortezapour1@gmail.com>
2023-09-28 14:47:46 +0330

    Use `Serilog` instead of built in logging service

--- CRUD_Application/Program.cs
+++ CRUD_Application/Program.cs
@@ -6,8 +6,17 @@ using Rotativa.AspNetCore;
 using RepositoryContracts;
 using Repositories;
 using Microsoft.AspNetCore.HttpLogging;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//Use serilog
+builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider services, LoggerConfiguration loggerConfiguration) =>
+{
+    loggerConfiguration
+    .ReadFrom.Configuration(context.Configuration) //read configuration settings from built-in IConfiguration
+    .ReadFrom.Services(services); //read out current app's services and make them available to serilog
+});
 
 //add services
 builder.Services.AddControllersWithViews();
@@ -15,15 +24,6 @@ builder.Services.AddScoped<IPersonsService, PersonsService>();
 builder.Services.AddScoped<ICountriesService, CountriesService>();
 builder.Services.AddScoped<ICountriesRepository, CountriesRepository>();
 builder.Services.AddScoped<IPersonsRepository, PersonsRepository>();
-
-//logging
-builder.Host.ConfigureLogging(logging =>
-{
-    logging.ClearProviders();
-    logging.AddConsole();
-    logging.AddDebug();
-    logging.AddEventLog();
-});
 
 //http logging options
 builder.Services.AddHttpLogging(options =>
